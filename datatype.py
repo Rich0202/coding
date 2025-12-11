873fe809d6c56d35646fecc7cdfeb40f566ac33d
Richa0202 <richapradeep1999@gmail.com>
2025-12-12 00:29:53 +0530

    Datatypes in python

---
+++ datatype.py
@@ -0,0 +1,28 @@
+# types of datatype we have total of two types in DT 1.primitive and 2 nd is non primitive
+# primitive data type consist of basic values 
+# String
+a = "Name"
+print(a);
+# int
+b = 10
+print(b);
+
+# float
+c = 33.44
+print(c);
+# bool
+d = True
+print(d);
+
+# Non primitive data type it consists of Sets, list, dict, complex numbers 
+li = ["a","v","c","f"]; 
+print(li); 
+tu = (1,33,66,88,99);
+print(tu);
+dict= {
+    "name": "Richa",
+    "age": 24,
+    "work":"employeed"
+};
+print(dict);
+
