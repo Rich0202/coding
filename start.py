9bb76fa7296edf26e119edc0f3ec167f5adc63b8
Richa0202 <richapradeep1999@gmail.com>
2025-12-12 00:15:34 +0530

    Addition of two numbers in python

--- start.py
+++ start.py
@@ -1,1 +1,9 @@
-print('Hello world')
+# Variable
+# rules of Variable
+# 1. Should not start with number 2, no spaces allowed, no special characters allowed apart from _ 
+#  write a script to add two numbers 
+a = 10;
+b = 20;
+
+add = a + b;
+print(add);
