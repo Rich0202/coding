0ccbcca728331c7e834ae4fcaf4e1a3a1036b0e4
Richa0202 <richapradeep1999@gmail.com>
2025-12-12 17:58:45 +0530

    Operators

---
+++ operators.py
@@ -0,0 +1,101 @@
+# types of operators
+# arithmetic operators  -- add+ 
+a = 5;
+b = 12;
+print(a + b);
+# sub -
+a = 5;
+b = 12;
+print(a - b);
+# multiplication *
+a = 5;
+b = 12;
+print(a * b);
+# division /
+a = 5;
+b = 12;
+print(a / b);
+# mod %
+a = 5;
+b = 12;
+print(a % b);
+
+
+# Logical operator
+# and 
+age = 5;
+resitent_status = 'Resident';
+if age >= 18 and resitent_status == 'Resident':
+    print('Yes u can vote');
+else:
+    print('You cant vote');
+
+# or operator
+
+color = "Red";
+color2 = 'blue';
+if color == 'Red' or color == 'Blue':
+    print('Yes it is fine');
+else:   
+    print('it is not fine');
+
+# not operator
+# is true 
+is_true = True;
+if  not is_true:
+    print('yes');
+else:
+    print('no')    
+
+# assignment operator
+a = 10;
+a += a;
+print(a);
+
+a = 10;
+a -= a;
+print(a);
+
+# comparision
+a = 10;
+b = 20;
+if a >= b :
+    print ('Yes');
+else :
+    print('No'); 
+
+# comparision
+a = 10;
+b = 20;
+if a == b :
+    print ('Yes');
+else :
+    print('No'); 
+
+a = 10;
+b = 20;
+if a <= b :
+    print ('Yes');
+else :
+    print('No');     
+
+a = 10;
+b = 20;
+if a != b :
+    print ('Yes');
+else :
+    print('No');   
+
+
+# nested if else statement
+if 2 > 10:
+    print('it is true');
+elif 2 == 10:
+    print('It is false');
+else :
+    print("no 2 < 10 ");
+
+
+
+
+
